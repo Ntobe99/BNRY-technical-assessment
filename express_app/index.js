ef95fb6934e978731346af9ccef8ec2a1813c957
Ntobe99 <ntobsie2010@gmail.com>
2023-04-20 14:52:44 +0200

    cors update2

--- express_app/index.js
+++ express_app/index.js
@@ -17,6 +17,7 @@ const app = express();
 app.use((req, res, next)=> {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
   res.header('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Methods", "*")
   res.header("Access-Control-Allow-Headers", "*")
