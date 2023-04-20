1f1af5b2593281d380404334b138eeb91f4af48d
Ntobe99 <ntobsie2010@gmail.com>
2023-04-20 16:13:56 +0200

    cors updated

--- express_app/index.js
+++ express_app/index.js
@@ -15,9 +15,8 @@ const PORT = process.env.PORT || 4000;
 dotenv.config();
 const app = express();
 app.use((req, res, next)=> {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
   res.header('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Methods", "*")
   res.header("Access-Control-Allow-Headers", "*")
