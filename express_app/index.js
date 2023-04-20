20ec7e2b4a93e9bf4165fb729b179ff1e36eb79e
Ntobe99 <ntobsie2010@gmail.com>
2023-04-20 14:46:17 +0200

    cors update

--- express_app/index.js
+++ express_app/index.js
@@ -12,9 +12,23 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 //port
 const PORT = process.env.PORT || 4000;
-
 dotenv.config();
 const app = express();
+app.use((req, res, next)=> {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next();
+});
+  app.use(
+  cors(), 
+  cookieParser(),
+  express.json,
+  express.urlencoded({extended: false})
+)
+
 // Get top headlines
 app.get('/news/headlines', async (req, res) => {
   try {
@@ -39,22 +53,6 @@ app.get('/news/search/:query', async (req, res) => {
 app.get("^/$|/BNRY-test", (req, res) => {
     res.status(200).sendFile(path.join(__dirname,'./view/index.html'));
   })
-
-  app.use((req, res, next)=> {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-    res.header("Access-Control-Allow-Credentials", "true")
-    res.header("Access-Control-Allow-Methods", "*")
-    res.header("Access-Control-Allow-Headers", "*")
-    next();
-});
-    app.use(
-    cors(), 
-    cookieParser(),
-    express.json,
-    express.urlencoded({extended: false})
-)
-
-
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
